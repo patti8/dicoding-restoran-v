ea04ac52ca7673515824b8ee257c399236a48be4
Tendy Rivaldo <pattipeilohyterry@gmail.com>
2021-05-16 08:53:02 +0900

    responsive apps

---
+++ src/scripts/index.js
@@ -0,0 +1,24 @@
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/main.css';
+import '../styles/mobile.css';
+
+// data restaurant
+import ('../DATA.json').then(({default: jsonData}) => {
+    console.log(jsonData);
+    let datas = jsonData['restaurants'];
+    let dataList = '';
+    datas.forEach(function(data){
+        dataList +=`
+        <div class="card">
+            <img class="img-fluid" src="${data['pictureId']}" alt="${data['name']}" title="${data['name']}" />
+            <div>${data['city']}</div>
+            <a href="#">${data['rating']}</a>
+            <div class="text-primary">${data['description']}</div>
+        </div>
+        `
+        
+    }) 
+    document.querySelector('#myResto').innerHTML = dataList;
+});
+
+console.log('Hello Coders! :)');
